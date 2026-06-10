b0899f872366e3d9de8d03703c68a44e6218d0c2
Paulo Rodrigo <paulofigueiredo@id.uff.br>
2026-06-09 21:01:25 -0300

    Nova implementação de cadastro

--- src/main/java/controller/RegisterServlet.java
+++ src/main/java/controller/RegisterServlet.java
@@ -17,15 +17,9 @@ public class RegisterServlet extends HttpServlet {
     protected void doOptions(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
 
-        // Permite requisições vindas do React
         resp.setHeader("Access-Control-Allow-Origin", "*");
-
-        // Métodos HTTP permitidos
         resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
-
-        // Cabeçalhos permitidos
         resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
         resp.setStatus(HttpServletResponse.SC_OK);
     }
 
@@ -34,40 +28,38 @@ public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
 
+        // Configuração da resposta como JSON
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
         // Configuração de CORS para comunicação com o React
         resp.setHeader("Access-Control-Allow-Origin", "*");
         resp.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
         resp.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-        // Obtém os dados enviados pelo formulário
         String name = req.getParameter("name");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
 
         try {
-
-            // Cria objeto de usuário com os dados recebidos
             User user = new User();
             user.setName(name);
             user.setEmail(email);
             user.setPassword(password);
 
-            // Salva o usuário no banco de dados
             UserDAO dao = new UserDAO();
             dao.save(user);
 
-            // Retorna sucesso para o front-end
             resp.setStatus(HttpServletResponse.SC_OK);
-            resp.getWriter().write("Usuário cadastrado com sucesso");
+            resp.getWriter().write(
+                    "{\"success\":true,\"message\":\"Usuário cadastrado com sucesso\"}");
 
         } catch (Exception e) {
-
-            // Registra erro no console do servidor
             e.printStackTrace();
 
-            // Retorna erro para o front-end
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            resp.getWriter().write("Erro ao cadastrar usuário: " + e.getMessage());
+            resp.getWriter().write(
+                    "{\"success\":false,\"message\":\"Erro ao cadastrar usuário\"}");
         }
     }
 }
