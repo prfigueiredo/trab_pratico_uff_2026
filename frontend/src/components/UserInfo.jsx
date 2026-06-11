ecf3ed4c1aeff7a7f2d5a3ba218613794150b230
Brenda <brendaliporaci4@gmail.com>
2026-06-11 12:55:24 -0300

    Estilização da página de saudação

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -1,7 +1,7 @@
 // Função para dar "bem vindo" com o nome do usuário
 function UserInfo({ name }) {
     return (
-        <h1>Bem-vindo, {name}</h1>
+        <h1>Bem-vindo,<br />{name}</h1>
     );
 }
 
